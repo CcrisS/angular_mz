6e9e09d3ec3552019dfc99a53856dea2cb5976bd
Cristina Sanz <cristina.sanz@colaboradoresunidadeditorial.es>
2015-10-20 09:53:45 +0200

    new structure

--- src/Product.php
+++ src/Product.php
@@ -5,10 +5,11 @@
  */
 class Product {
 
+    const IMG_BASE_PATH = '/angular_mz/app/img/';
     const MAX_CSS_WIDTH_CLASS = 1; // 1..4
 
     protected static $colors = array('#8E44AD', '#F39C12', '#D35400', '#006A3F', '#2980B9', '#C0392B', '#956DC9', '#27AE60');
-    protected static $imgs = array('img/product1.jpg', 'img/product2.jpg', 'img/product3.jpg', 'img/product4.jpg', 'img/product5.jpg');
+    protected static $imgs = array('product1.jpg', 'product2.jpg', 'product3.jpg', 'product4.jpg', 'product5.jpg');
 
     protected $cssWidthClass;
     protected $bgColor;
@@ -32,7 +33,7 @@ class Product {
         $this->name = 'Product '. rand(1000, 9999);
         $this->cssWidthClass = 'item'.rand(1, self::MAX_CSS_WIDTH_CLASS);
         $this->bgColor = self::$colors[rand(0, count(self::$colors) - 1)];
-        $this->img = self::$imgs[rand(0, count(self::$imgs) - 1)];
+        $this->img = self::IMG_BASE_PATH . self::$imgs[rand(0, count(self::$imgs) - 1)];
         $this->price = rand(0, 99999) / 100;
         $this->text = file_get_contents('http://loripsum.net/api/1/short/plaintext');
 
